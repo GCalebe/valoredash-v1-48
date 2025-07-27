dc7347e62514639d48cd95023d8bedaba75b2d24
GABRIEL CALEBE DA SILVA ALVES <gabrielklebe2@gmail.com>
2025-07-27 11:19:15 -0300

    updaee 3x2 dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -52,7 +52,7 @@ const Dashboard = React.memo(() => {
           Painel Administrativo
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 max-w-7xl mx-auto w-full mb-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-4xl mx-auto w-full mb-8">
           {dashboardCards}
         </div>
         
