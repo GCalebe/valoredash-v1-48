7048348f413e823ea44c7372161763ecc2e0e081
GABRIEL CALEBE DA SILVA ALVES <gabrielklebe2@gmail.com>
2025-08-08 02:53:12 -0300

    remover oq não tava usando

--- src/components/clients/tree-views/index.ts
+++
@@ -1,6 +0,0 @@
-// Exportações dos componentes de visualização em árvore
-export { default as ClientTreeView2 } from './ClientTreeView2';
-export { default as ClientTreeView4 } from './ClientTreeView4';
-
-// Exportação do tipo Contact para facilitar importações
-export type { Contact } from '@/types/client';
